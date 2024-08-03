a48bd9f63ab963707f54d92c1c290d6c6b75f918
femamanic <femamanic@unjbg.edu.pe>
2024-08-03 13:43:08 -0500

    Agregar GerenteDeTienda.h: define todas las funciones del Gerente de Tienda (STM).

---
+++ GerenteDeTienda.h
@@ -0,0 +1,10 @@
+#ifndef GERENTEDETIENDA_H
+#define GERENTEDETIENDA_H
+
+void menuGerenteDeTienda();
+void AgregarProducto();
+void EliminarProducto();
+void verRegistroDeInventario();
+void AgregarPromociones();
+
+#endif // !GERENTEDETIENDA_H
