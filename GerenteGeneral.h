da4879465c4852915d6564bedbf784f582612eb2
femamanic <femamanic@unjbg.edu.pe>
2024-08-03 13:36:08 -0500

    Agregar GerenteGeneral.h: define todas las funciones del Gerente General (CEO).

---
+++ GerenteGeneral.h
@@ -0,0 +1,11 @@
+#ifndef GERENTEGENERAL_H
+#define GERENTEGENERAL_H
+
+void menuGerenteGeneral();
+void Contratar();
+void Despedir();
+void verRegistroDeEmpleados();
+void verReportesDeInventario();
+void verReportesDeVentas();
+
+#endif // GERENTEGENERAL_H
