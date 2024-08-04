1fc2add1d7b59910dda162e0e913dfc31b6c1738
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-08-03 23:44:12 -0500

    agregar menuGerenteGeneral.cpp: se implemento algunos mensajes dirigidos a usuario y tambien se agrego una estructura de seleccion

--- menuGerenteGeneral.cpp
+++ menuGerenteGeneral.cpp
@@ -5,5 +5,33 @@ using namespace std;
 void menuGerenteGeneral(){
     int n;
     do{
+        cout<<"------Bienvenido------\n";
+        cout<<"1: contratar un nuevo empleado\n";
+        cout<<"2: despedir personal.\n";
+        cout<<"3: ver los datos de los empleados.\n";
+        cout<<"4: ver los productos almacenados en la empresa.\n";
+        cout<<"5: ver el registro de compras de la empresa.\n";
+        cout<<"6: ver el registro de ventas de la empresa.\n";
+        cout<<"0: salir del de la funcion.\n";
+        cout<<"ingrese la opcion a seleccionar: ";cin>>n;
+        switch (n)
+        {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        default:
+            cout<<"----ERROR----\n";
+            cout<<"ingrese una opcion valida.\n";
+            break;
+        }
     }while(n!=0);
-}
+} 
