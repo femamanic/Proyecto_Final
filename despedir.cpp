c81826b82ae939bfd0988a3d6b7d94cd67a16b59
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-08-04 17:11:12 -0500

    Agregar despedir.cpp:se implemento algunos mensajes a la funcion pero todavia sigue en desarrollo

--- despedir.cpp
+++ despedir.cpp
@@ -1,6 +1,13 @@
 #include "GerenteGeneral.h"
 #include <iostream>
+#include "structs.h"
 using namespace std;
 void Despedir(){
-    int n;
+    int n,o;
+    cout<<"-----funcion de despedir empleados-----\n";
+    cout<<"el empledo a despedir es: ";cin>>n;
+    cout<<"Desea eliminar a alguien mas? "<<endl;
+    cout<<"1: si."<<endl;
+    cout<<"0: No."<<endl;
+    cout<<"ingrese la opcion a seleccionar: ";cin>>o;
 }
