e90484f96fd662144bb1500c723078559ae73577
femamanic <femamanic@unjbg.edu.pe>
2024-08-04 20:17:53 -0500

    AprobarCheques.cpp: agregar función para aprobar cheques del contador(a).

---
+++ AprobarCheques.cpp
@@ -0,0 +1,18 @@
+#include "Contador.h"
+#include "structs.h"
+#include <iostream>
+
+using namespace std;
+
+void AprobarCheques() {
+    for (int i = 0; i < nEmpleados, i++){
+        bool aprobar
+        cout << "Codigo:\t" << empleado[i].ID
+        cout << "Nombre:\t" << empleado[i].nombre;
+        cout << "DNI:\t" << empleado[i].DNI;
+        cout << "Sueldo base:\t" << empleado[i].sueldo
+        cout << "Minutos tarde:\t" << empleado[i].minTarde
+        cout << "Bonificacion por ventas:\t" << empleado[i].ventas
+        cout << "Sueldo final:\t" << empleado
+    }    
+}
