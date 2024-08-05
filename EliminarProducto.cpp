983afc0deca04dd325b31cd515df3b0d57468448
femamanic <femamanic@unjbg.edu.pe>
2024-08-05 05:10:58 -0500

    Agregar EliminarProductos.cpp: se creo la funcion para eliminar un producto.

--- EliminarProducto.cpp
+++ EliminarProducto.cpp
@@ -1,25 +1,21 @@
 #include "GerentedeTienda.h"
-#include<iostream>
-#include <string>
+#include "structs.h"
+#include <iostream>
 using namespace std;
 
 void EliminarProducto(){
-int codigo;
-    cout << "Ingrese el codigo del producto que desea eliminar: ";cin >> codigo;
-    int index = -1;
-    for(int i = 0; i < nProductos; i++) {
-        if(producto[i].codigo == codigo) {
-            index = i;
-            break;
+    string eliminar;
+    cout << "Ingrese el nombre del producto que desea eliminar: "; cin >> eliminar;
+    for(int i = 0; i < nProductos ; i++){
+        if(producto[i].nombre == eliminar){
+            cout << "Producto eliminado." << endl;
+            cout << "ID: " << producto[i].ID << endl;
+            cout << "Nombre: " << producto[i].nombre << endl;
+            for(int j = i; j < nProductos; j++){
+                producto[j] = producto[j+1];
+            }
+            nProductos = nProductos - 1;
+            return;
         }
     }
-    if(index == -1){
-        cout<<"Producto no encontrado."<<endl;
-        return;
-    }
-    for(int i = index; i < nProductos - 1; i++){
-        producto[i] = producto[i+1];
-    }
-    nProductos--;
-    cout << "Producto eliminado con exito."<<endl;
 }
