fcc285e9fa225cb256dabf17f7b548c124d3b7f7
femamanic <femamanic@unjbg.edu.pe>
2024-08-05 04:39:13 -0500

    Actualizar PF.cpp: se creo la ruta a todos los menus

--- PF.cpp
+++ PF.cpp
@@ -4,31 +4,39 @@
 #include "GerenteDeTienda.h"
 #include "Contador.h"
 #include "DepartamentoDeTesoreria.h"
+#include "database.h"
+#include "CajaRegistradora.h"
 using namespace std;
 
 int main () {
-    system("cls");
+    basedatosEmpleados();
+    basedatosProductos();
     string user, pass;
-    empleado[0].ID = "CEO-001";
-    empleado[0].contra = "12345";
-    cout << "ingresar" << "\n";
-    cout << "ID: "; cin >> user;
-    cout << "Password: "; cin >> pass;
-    
-    bool confirmar = false;
+    do {
+        system("cls");
+        cout << "ingresar" << "\n";
+        cout << "ID: "; cin >> user;
+        cout << "Password: "; cin >> pass;
+        
+        bool confirmar = false;
 
-    for (int i = 0; i < 50; i++) {
-        if (user == empleado[i].ID && pass == empleado[i].contra) {
-            cout << "Bienvenido\n";
-            confirmar = true;
-            break;
+        for (int i = 0; i < 50; i++) {
+            if (user == empleado[i].ID && pass == empleado[i].contra) {
+                cout << "Bienvenido\n";
+                confirmar = true;
+                break;
+            }
         }
-    }
-    if (confirmar == false) {
-        cout << "ID o Password incorrecta\n";
-    }
-    
-    if (user.substr(0, 3) == "CEO") {
-        menuGerenteGeneral();
-    }
+        if (confirmar == false) {
+            cout << "ID o Password incorrecta\n";
+        } else if (user.substr(0, 3) == "CEO") {
+            menuGerenteGeneral();
+        } else if (user.substr(0, 3) == "SMT") {
+            menuGerenteDeTienda();
+        } else if (user.substr(0, 3) == "ACC") {
+            menuContador();
+        } else if (user.substr(0, 3) == "CAS") {
+            menuCajaRegistradora();
+        }
+    } while (true);
 }
