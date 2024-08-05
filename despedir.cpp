2b6d3b2fb51c4cc0530f69b2bc07624ecf54f889
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-08-04 19:53:38 -0500

    Agregar despedir.cpp: se implemento distintas funciones pero aun esta sometido a posibles cambios

--- despedir.cpp
+++ despedir.cpp
@@ -4,10 +4,22 @@
 using namespace std;
 void Despedir(){
     int n,o;
+    char a;
     cout<<"-----funcion de despedir empleados-----\n";
-    cout<<"el empledo a despedir es: ";cin>>n;
-    cout<<"Desea eliminar a alguien mas? "<<endl;
-    cout<<"1: si."<<endl;
-    cout<<"0: No."<<endl;
-    cout<<"ingrese la opcion a seleccionar: ";cin>>o;
+    cout<<"elija el numero de empleado que desea despedir: "<<endl;
+    for(int i=0;i<nEmpleados;i++){
+        cout<<i+1<<": "<<empleado[i].nombre<<endl;
+    }
+    do{
+        cout<<"eliminar"<<endl;
+        cout<<"1: si."<<endl;
+        cout<<"0: No."<<endl;
+        cout<<"ingrese la opcion a seleccionar: ";cin>>n;
+        cout<<"el empledo a despedir es: ";cin>>o;
+        o=n-1;
+        empleado[o].nombre=a;
+        empleado[o].ID=a;
+        empleado[o].contra=a;
+        empleado[o].sueldo=a;
+    }while(n!=0);
 }
