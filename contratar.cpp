5e28e0a5384a6d8017210c8a156fc90601858433
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-08-04 19:51:45 -0500

    Agregar contratar.cpp: se implemento distintos mensajes pero aun esta sometido a posibles cambios

--- contratar.cpp
+++ contratar.cpp
@@ -10,10 +10,10 @@ void Contratar(){
     for(int i=0;i<n;i++){
         nEmpleados=nEmpleados+1;
         cin.ignore( );
-        cout<<"ingrese el ID del nuevo trabajador: ";getline(cin, registro[nEmpleados].ID);
-        cout<<"ingrese el nombre: "; getline(cin, registro[nEmpleados].nombre );
-        cout<<"ingrese la contraseña: ";getline(cin, registro[nEmpleados].contra);
-        cout<<"ingrese el sueldo que se designara a este nuevo trabajador: ";cin>>registro[nEmpleados].sueldo;
+        cout<<"ingrese el ID del nuevo trabajador: ";getline(cin, empleado[nEmpleados].ID);
+        cout<<"ingrese el nombre: "; getline(cin, empleado[nEmpleados].nombre );
+        cout<<"ingrese la contraseña: ";getline(cin, empleado[nEmpleados].contra);
+        cout<<"ingrese el sueldo que se designara a este nuevo trabajador: ";cin>>empleado[nEmpleados].sueldo;
         cout<<"----se ha registrado correctamente----"<<endl;
     }
 }
