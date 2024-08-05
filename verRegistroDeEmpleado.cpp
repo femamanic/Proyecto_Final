4d9f2cd2eaf89bd8a37179ec8777b7c274cb0851
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-08-04 19:46:18 -0500

    Agregar verRegistroDeEmpleado.cpp: se implemento un implemento la funcion pero aun esta sometida a posibles cambios

--- verRegistroDeEmpleado.cpp
+++ verRegistroDeEmpleado.cpp
@@ -4,5 +4,8 @@
 using namespace std;
 void verRegistroDeEmpleados(){
     for(int i=0;i<nEmpleados;i++){
+        cout<<"el ID del empleado es: "<<empleado[i].ID<<endl;
+        cout<<"el nombre del empleado es: "<<empleado[i].nombre<<endl;
+        cout<<"el sueldo del empleado es: "<<empleado[i].sueldo<<endl;
     }
 }
