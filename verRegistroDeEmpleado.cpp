8c194a232588e54379151cab0cb8f44de33ddf5c
femamanic <femamanic@unjbg.edu.pe>
2024-08-05 05:23:35 -0500

    Actualizar verRegistroDeEmpleado.cpp: se agregaron mas datos a imprimir.

--- verRegistroDeEmpleado.cpp
+++ verRegistroDeEmpleado.cpp
@@ -4,8 +4,11 @@
 using namespace std;
 void verRegistroDeEmpleados(){
     for(int i=0;i<nEmpleados;i++){
-        cout<<"el ID del empleado es: "<<empleado[i].ID<<endl;
-        cout<<"el nombre del empleado es: "<<empleado[i].nombre<<endl;
-        cout<<"el sueldo del empleado es: "<<empleado[i].sueldo<<endl;
+        cout<<"El ID del empleado es: "<<empleado[i].ID<<endl;
+        cout<<"El nombre del empleado es: "<<empleado[i].nombre<<endl;
+        cout<<"El DNI del empleado es: "<<empleado[i].DNI<<endl;
+        cout<<"El sueldo del empleado es: "<<empleado[i].sueldo<<endl;
+        cout<<"El cargo del empleado es: "<<empleado[i].cargo<<endl;
+        cout<<"----------------------------------------------------"<<endl;
     }
 }
