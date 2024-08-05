b74875ce8f70c761ae966dfa0e46e6a63fe2d138
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-08-04 22:25:27 -0500

    Agregar menuGerenteGeneral.cpp: se agrego unos conchetes a los numeros para que se vean algo mas esteticos

--- menuGerenteGeneral.cpp
+++ menuGerenteGeneral.cpp
@@ -6,13 +6,13 @@ void menuGerenteGeneral(){
     int n,b=0;
     do{
         cout<<"------Bienvenido------\n";
-        cout<<"1: contratar un nuevo empleado\n";
-        cout<<"2: despedir personal.\n";
-        cout<<"3: ver los datos de los empleados.\n";
-        cout<<"4: ver los productos almacenados en la empresa.\n";
-        cout<<"5: ver el registro de compras de la empresa.\n";
-        cout<<"6: ver el registro de ventas de la empresa.\n";
-        cout<<"0: salir del de la funcion.\n";
+        cout<<"[1]: contratar un nuevo empleado\n";
+        cout<<"[2]: despedir personal.\n";
+        cout<<"[3]: ver los datos de los empleados.\n";
+        cout<<"[4]: ver los productos almacenados en la empresa.\n";
+        cout<<"[5]: ver el registro de compras de la empresa.\n";
+        cout<<"[6]: ver el registro de ventas de la empresa.\n";
+        cout<<"[0]: salir del de la funcion.\n";
         cout<<"ingrese la opcion a seleccionar: ";cin>>n;
         switch (n)
         {
