2880e695ea96a264620b994b1de064bec07a901d
femamanic <femamanic@unjbg.edu.pe>
2024-08-05 05:03:31 -0500

    Agregar BuscarProducto.cpp: opcion para que el cajero encuentre un producto en especifico.

--- BuscarProducto.cpp
+++ BuscarProducto.cpp
@@ -1,6 +1,19 @@
-#include"Empleado.h"
+#include "CajaRegistradora.h"
+#include "structs.h"
+#include <iostream>
+using namespace std;
 
 void BuscarProducto(){
-cout << "-------Buscar_Productos-------"<< endl;
-cout << "1. "
+    string buscar;
+    cout << "Ingrese el nombre del producto que desea buscar: "; cin >> buscar;
+    for(int i = 0; i < nProductos ; i++){
+        if(producto[i].nombre == buscar){
+            cout << "Producto encontrado." << endl;
+            cout << "ID: " << producto[i].ID << endl;
+            cout << "Nombre: " << producto[i].nombre << endl;
+            cout << "Precio: " << producto[i].precio << endl;
+            cout << "Cantidad: " << producto[i].unidades << endl;
+            return;
+        }
+    }
 }
